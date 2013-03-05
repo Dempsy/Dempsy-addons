f903e6a2a0bc713d0cbfadf145d7825ce36c5ee0
Jim Carroll <thecarrolls@jiminger.com>
2013-03-05 15:11:09 -0500

    Move the 0mq work into the Dempsy-addons project.

---
+++ lib-dempsy0mq/src/test/java/com/nokia/dempsy/messagetransport/zmq/TestZmqTransport.java
@@ -0,0 +1,84 @@
+package com.nokia.dempsy.messagetransport.zmq;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import com.nokia.dempsy.TestUtils;
+import com.nokia.dempsy.messagetransport.Destination;
+import com.nokia.dempsy.messagetransport.Listener;
+import com.nokia.dempsy.messagetransport.MessageTransportException;
+import com.nokia.dempsy.messagetransport.Receiver;
+import com.nokia.dempsy.messagetransport.Sender;
+import com.nokia.dempsy.messagetransport.SenderFactory;
+
+public class TestZmqTransport
+{
+   public static final int baseTimeoutMillis = 20000;
+   
+   @Test
+   public void testZmqServerStart() throws Throwable
+   {
+      ZmqTransport transport = new ZmqTransport();
+      Receiver r = null;
+      try
+      {
+         r = transport.createInbound(null,null);
+         r.setListener(new Listener() { 
+            @Override public void transportShuttingDown(){}
+            @Override public boolean onMessage(byte[] messageBytes, boolean failFast) { return true; }
+         });
+         r.start();
+      }
+      finally
+      {
+         if (r != null) r.shutdown();
+      }
+   }
+   
+   @Test
+   public void testZmqSendSimpleMessage() throws Throwable
+   {
+      ZmqTransport transport = new ZmqTransport();
+      Receiver r = null;
+      SenderFactory senderFactory = null;
+      try
+      {
+         r = transport.createInbound(null,null);
+      
+         final AtomicReference<String> lastReceived = new AtomicReference<String>();
+         r.setListener(new Listener()
+         {
+            @Override public void transportShuttingDown() { }
+
+            @Override
+            public boolean onMessage(byte[] messageBytes, boolean failFast) throws MessageTransportException
+            {
+               lastReceived.set(new String(messageBytes));
+               return true;
+            }
+         });
+         r.start();
+
+         Destination d = r.getDestination();
+
+         senderFactory = transport.createOutbound(null, null, null);
+         Sender s = senderFactory.getSender(d);
+
+         s.send("Hello".getBytes());
+
+         assertTrue(TestUtils.poll(baseTimeoutMillis, lastReceived, new TestUtils.Condition<AtomicReference<String>>()
+         {
+            @Override
+            public boolean conditionMet(AtomicReference<String> o) { return "Hello".equals(o.get()); }
+         }));
+      }
+      finally
+      {
+         if (r != null) r.shutdown();
+         if (senderFactory != null) senderFactory.shutdown();
+      }
+   }
+
+}
